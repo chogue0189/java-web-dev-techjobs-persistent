83ab3caaa20ffbffb76cc3a725c1de9956db7546
cameron <chogue0189@gmail.com>
2020-03-30 23:09:34 -0500

    Completed

--- queries.sql
+++ queries.sql
@@ -1,7 +1,15 @@
 ## Part 1: Test it with SQL
-
+SELECT * FROM job;
+SELECT * FROM skill;
+SELECT * FROM employer;
+SELECT * FROM job_skills;
 ## Part 2: Test it with SQL
+SELECT * FROM employer WHERE location="St. Louis";
 
 ## Part 3: Test it with SQL
+DROP TABLE `techjobs`.`job`;
 
 ## Part 4: Test it with SQL
+SELECT skill.name,skill.description FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY skill.name;
